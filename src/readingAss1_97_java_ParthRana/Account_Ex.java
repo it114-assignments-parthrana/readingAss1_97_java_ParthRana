9c80eca9a1e91beb04772b6e99e65d090aad89b2
pr279 <pr279@njit.edu>
2016-11-21 01:11:33 -0500

    First Commit Message

---
+++ src/readingAss1_97_java_ParthRana/Account_Ex.java
@@ -0,0 +1,29 @@
+/**
+ * 
+ */
+package readingAss1_97_java_ParthRana;
+
+/**
+ * @author granzer
+ *
+ */
+
+public class Account_Ex {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		Account account = new Account (1122, 20000);
+		account.setAnnualIntrestRate(4.5);
+		account.withdraw(2500);
+		account.deposit(3000);
+		
+		System.out.println("\n                  Account");
+		System.out.println("AccountID: " + account.getId());
+		System.out.println("Data created: "+ account.getId());
+		System.out.printf("Balance: $%.2f\n", account.getBalance());
+		System.out.printf("Monthly intrest: $%.2f\n", account.getMonthlyIntrest());
+		// TODO Auto-generated method stub
+	}
+}
